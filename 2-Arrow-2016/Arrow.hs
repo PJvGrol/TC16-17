80a50c158ff3dbd2063e203c051c290716438edb
PJvGrol <pjvgrol@xs4all.nl>
2016-12-21 14:41:20 +0100

    Ex 8 + 9 (Part)

--- 2-Arrow-2016/Arrow.hs
+++ 2-Arrow-2016/Arrow.hs
@@ -158,4 +158,20 @@ printContent Boundary = '#'
 
 -- Exercise 8
 toEnvironment :: String -> Environment
-toEnvironment s = check ((parsehap . scan) s)
+toEnvironment s = f
+                where
+                rs = (parsehap . scan) s
+                c = check rs
+                f | c = progToEnv rs
+                  | otherwise = L.empty
+                  
+progToEnv :: Program -> Environment
+progToEnv p = L.empty
+
+-- Exercise 9
+step :: Environment -> ArrowState -> Step
+step env (ArrowState sp pos hd st) | stackIsEmpty st = Done sp pos hd
+                                   | otherwise = undefined
+
+stackIsEmpty :: Stack -> Bool
+stackIsEmpty st = True 
