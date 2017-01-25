ab5ecdb4eb83148534f936e9a3a6e03877d718f8
PJvGrol <pjvgrol@xs4all.nl>
2017-01-25 13:56:22 +0100

    Removing Comments

--- 3-CSharp-SSM-2016/src/CSharpCode.hs
+++ 3-CSharp-SSM-2016/src/CSharpCode.hs
@@ -12,7 +12,7 @@ import Data.Char
 data ValueOrAddress = Value | Address
     deriving Show
 
-type Env = Map Token (Loc, Int)--[(Token,(Loc,Int))]
+type Env = Map Token (Loc, Int)
 
 data Loc = Mem | Lcl | Param
         deriving Eq
@@ -28,12 +28,8 @@ codeAlgebra =
 fClas :: Token -> [Code] -> Code
 fClas c ms = [Bsr "main", HALT] ++ concat ms
 
-numberOfDecls :: Member -> Int
-numberOfDecls = undefined --(MemberM )
-
 fMembDecl :: Decl -> Code
 fMembDecl d = [TRAP 0]
-
 
 fMembMeth :: Type -> Token -> [Decl] -> (Env -> Env -> (Env, Code)) -> Code
 fMembMeth t (LowerId x) ps s = [LABEL x, LINK (length env2 - length ps)] ++ snd(s env env2) ++ [UNLINK, RET] 
@@ -42,45 +38,15 @@ fMembMeth t (LowerId x) ps s = [LABEL x, LINK (length env2 - length ps)] ++ snd(
                                    env = Prelude.foldr op2 empty ps
                                    op2 (Decl tp tk) mp = M.insert tk (Lcl,(-1)*(size mp)-2) mp
                                    (env2,code) = s env env2
--- TODO: something with ps
- -- Ga variabelen opslaan, houd bij waar ze opgeslagen staan
-{-=======
-fMembMeth :: Type -> Token -> [Decl] -> Code -> Code
-fMembMeth t (LowerId x) ps s = [LABEL x] ++ s ++ [STS (-n)] ++ [AJS (-(n-1))] ++ [RET] --TODO: Finish it
-                             where
-                             n = length ps
-                             envmap = undefined --fromList (zip (Prelude.map declToToken ps) (zip (repeat Lcl) [n..])) -- Lcl [(Token, (Loc, Int))]
-
-declToToken :: Decl -> Token
-declToToken (Decl tp tk) = tk-}
-
--- Environment is mapping van String naar Int (int relatieve locatie aan mp) laatste mp staat op -2
--- TODO: something with ps
--- Decl Type Token
--- Ga variabelen opslaan, houd bij waar ze opgeslagen staan -> Zet param onder markpointer mbv ldc
--- Link om MP te plaatsen
--- Environment om bij te houden waar de param opgeslagen staan
--- LDL: Waarde tov MP
-
-
- 
- 
 
 fStatDecl :: Decl -> Env -> Env -> (Env,Code)
-fStatDecl (Decl t tk) env env2 = let (env, code) = (M.insert tk (Lcl, nrOfLoc Lcl env) env, [LABEL (show tk), AJS 1])--((tk,(Lcl,length env + 1)):env)
+fStatDecl (Decl t tk) env env2 = let (env, code) = (M.insert tk (Lcl, nrOfLoc Lcl env) env, [LABEL (show tk), AJS 1])
                                  in (env, code)
--- length env + 1 moet anders -> param op -2, -3 etc, locals op 1, 2, 3 etc.
-                                 
-                                 
-f :: Env -> Code
-f = undefined
-                  
+
+
 fStatExpr :: (Env -> ValueOrAddress -> Code) -> Env -> Env -> (Env,Code)
-fStatExpr exp env env2 = (env, exp env2 Value) {-(f env, e Value ++ [pop])-}
-
-
-
-                            
+fStatExpr exp env env2 = (env, exp env2 Value)
+                   
 nrOfLoc :: Loc -> Env -> Int
 nrOfLoc loc env = size (fst (M.partition (\(x,y)-> x == loc) env))
                             
@@ -119,22 +85,17 @@ fExprCon (KeyFalse) env va = [LDC 0]
 fExprCon (UpperCh c) env va = [LDC (ord c)]
 fExprCon (LowerCh c) env va = [LDC (ord c)]
 
-
 fExprVar :: Token -> Env -> ValueOrAddress -> Code
 fExprVar t env va = case va of
                         Value -> [LDL (snd(env ! t))]
-                        Address -> [LABEL (show t), LABEL ((show.size) env)]--[STL (snd(env ! t))]
-                                    
-
-{-let loc = 37 in case va of
-                                              Value    ->  [LDL  loc]
-                                              Address  ->  [LDLA loc]-}
-{-fExprVar t@(LowerId x) env va = undefined let loc = fromJust (lookup t env) in case va of
-                                                           Value    ->  (,[LDL  loc])
-                                                           Address  ->  (,[LDLA loc]) -}
+                        Address -> [LABEL (show t), LABEL ((show.size) env)]
 
 fExprOp :: Token -> (Env -> ValueOrAddress -> Code) -> (Env -> ValueOrAddress -> Code) -> Env -> ValueOrAddress -> Code
-fExprOp (Operator "=") e1 e2 env va = e2 env Value ++ e1 env Address
+fExprOp (Operator "=") e1 e2 env va = e2 env Value ++ e1 env Address -- x = 3
+-- fExprOp (Operator "+=") e1 e2 env va = e1 env Value ++ e2 env Value ++ [ADD] ++ e1 env Address
+-- fExprOp (Operator "-=") e1 e2 env va = e1 env Value ++ e2 env Value ++ [SUB] ++ e1 env Address
+-- fExprOp (Operator "*=") e1 e2 env va = e1 env Value ++ e2 env Value ++ [MUL] ++ e1 env Address
+-- fExprOp (Operator "/=") e1 e2 env va = e1 env Value ++ e2 env Value ++ [DIV] ++ e1 env Address
 fExprOp (Operator op)  e1 e2 env va = e1 env Value ++ e2 env Value ++ [opCodes ! op]
 
 fMethCall :: Token -> [Env -> ValueOrAddress -> Code] -> Env -> ValueOrAddress -> Code
@@ -146,22 +107,3 @@ opCodes = fromList [ ("+", ADD), ("-", SUB),  ("*", MUL), ("/", DIV), ("%", MOD)
                    , ("<=", LE), (">=", GE),  ("<", LT),  (">", GT),  ("==", EQ)
                    , ("!=", NE), ("&&", AND), ("||", OR), ("^", XOR)
                    ]
-{-=======
-fExprOp :: Token -> (ValueOrAddress -> Code) -> (ValueOrAddress -> Code) -> ValueOrAddress -> Code
-fExprOp (Operator "=") e1 e2 va = e2 Value ++ [LDS 0] ++ e1 Address ++ [STA 0] -- x = 3 LDS = LoadStack: laad value op SP STA: 
--- LDC 3 LDC1 ADD LDS 0 LDLA 37 STA 0 
-{-fExprOp (Operator "+=") e1 e2 va = -- Laadt waarde van e1 ++ e2 Value ++ [ADD, LDS 0] ++ e1 Address ++ [STA 0]
-fExprOp (Operator "-=") e1 e2 va = -- Laadt waarde van e1 ++ e2 Value ++ [SUB, LDS 0] ++ e1 Address ++ [STA 0]
-fExprOp (Operator "*=") e1 e2 va = -- Laadt waarde van e1 ++ e2 Value ++ [MUL, LDS 0] ++ e1 Address ++ [STA 0]
-fExprOp (Operator "/=") e1 e2 va = -- Laadt waarde van e1 ++ e2 Value ++ [DIV, LDS 0] ++ e1 Address ++ [STA 0]-}
-fExprOp (Operator op)  e1 e2 va = e1 Value ++ e2 Value ++ (opCodes ! op)
-
-
-opCodes :: Map String [Instr]
-opCodes = fromList [ ("+", [ADD]), ("-", [SUB]),  ("*", [MUL]), ("/", [DIV]), ("%", [MOD])
-                   , ("<=", [LE]), (">=", [GE]),  ("<", [LT]),  (">", [GT]),  ("==", [EQ])
-                   , ("!=", [NE]), ("&&", [AND]), ("||", [OR]), ("^", [XOR]), ("++", [LDC 1, ADD])
-                   , ("--", [LDC 1, SUB])
-                   ] -- ++ += *= -- -= -- Aparte fExprOp voor alle operaties??
->>>>>>> origin/master-}
-
